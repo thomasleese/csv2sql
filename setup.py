9aad7db8512393c8d55de5ec00514e18e7a229f7
Tom Leese <inbox@tomleese.me.uk>
2014-12-21 11:30:01 +0000

    Remove unnecessary gumpf

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import ez_setup
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
+
 setup(
     name='csv2sql',
-    version='1.0.2',
-    keywords='csv sql',
+    version='1.0.0',
+    keywords='csv sql converter',
     author='Tom Leese',
     author_email='inbox@tomleese.me.uk',
     url='https://github.com/tomleese/csv2sql',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(),
     entry_points={
         'console_scripts': ['csv2sql = csv2sql.__main__:main']
     },
-    include_package_data=True,
     zip_safe=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Topic :: Utilities'
-    ],
-    test_suite='tests'
+    ]
 )
